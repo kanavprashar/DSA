f57047d6ec0a85455214e5d902ede6de00cabe11
Kanav Prashar <kanav013@gmail.com>
2025-04-02 20:20:40 +0530

    initial commit

---
+++ src/arraytraversal.java
@@ -0,0 +1,16 @@
+public class arraytraversal{
+    public static void main(String[]args){
+      int[] numbers = new int[5];
+      String[] names={"Kanav","arnav","pranav","manav","rimorav"};
+        System.out.println("number array");
+        for(int i=0;i<numbers.length;i++) {
+            System.out.println(numbers[i]);
+        }
+        System.out.println("Names Array");
+        for(int i=0;i<names.length;i++) {
+                System.out.println(names[i]);
+        }
+
+    }
+
+};
