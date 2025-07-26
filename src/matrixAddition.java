9174650861a6415584ff0dea265e88ae06c41987
Kanav Prashar <kanav013@gmail.com>
2025-07-27 00:45:47 +0530

    array

---
+++ src/matrixAddition.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class matrixAddition {
+
+    static void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void add(int[][] a, int r1, int c1, int[][] b, int r2, int c2) {
+        if (r1 != r2 || c1 != c2) {
+            System.out.println("Matrix addition not possible - dimensions don't match");
+            return;
+        }
+
+        int[][] sum = new int[r1][c1];
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {  // Fixed: was c2, should be c1
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        System.out.println("Sum of matrices:");
+        print(sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sd = new Scanner(System.in);
+
+        System.out.println("Enter the rows in matrix A:");
+        int r1 = sd.nextInt();
+        System.out.println("Enter the columns in matrix A:");
+        int c1 = sd.nextInt();
+
+        int[][] a = new int[r1][c1];
+        System.out.println("Enter elements of matrix A:");
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                a[i][j] = sd.nextInt();
+            }
+        }
+
+        System.out.println("Enter the rows in matrix B:");
+        int r2 = sd.nextInt();
+        System.out.println("Enter the columns in matrix B:");
+        int c2 = sd.nextInt();
+
+        int[][] b = new int[r2][c2];
+        System.out.println("Enter elements of matrix B:");
+        for (int i = 0; i < r2; i++) {  // Fixed: was r1, should be r2
+            for (int j = 0; j < c2; j++) {
+                b[i][j] = sd.nextInt();
+            }
+        }
+
+        System.out.println("Matrix A:");
+        print(a);
+        System.out.println("Matrix B:");
+        print(b);
+
+        // Call the add method to perform addition and display result
+        add(a, r1, c1, b, r2, c2);
+
+        sd.close();
+    }
+}
