96182502d20b8e54f271c0e751198d9a16b47128
Kanav Prashar <kanav013@gmail.com>
2025-08-06 16:10:57 +0530

    string

---
+++ src/string/StringBuilders.java
@@ -0,0 +1,37 @@
+package string;
+import java.util.*;
+import java.lang.*;
+
+public class StringBuilders {
+
+    public static void main(String[] args) {
+//        StringBuilder str=new StringBuilder("Kanav");
+////        str.append("Prashar");
+//
+//        //for kanav=>manav
+//        str.setCharAt(0,'m');
+////        System.out.println(str);
+//
+//        //insrt a charcter in the string
+//        str.insert(2,'y');
+//        System.out.println(str);
+//
+//        //delete charcter
+//        str.deleteCharAt(2);
+//        System.out.println(str);
+
+        //Reverse
+//        StringBuilder str =new StringBuilder("kanav");
+//        System.out.println(str);
+//        str.reverse();
+//        System.out.println(str);
+
+
+
+
+    }
+
+
+
+
+}
