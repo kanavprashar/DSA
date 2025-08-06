40ec74b7f4fa5cb21d81a5803f5b7161fc7e319b
Kanav Prashar <kanav013@gmail.com>
2025-08-06 16:11:06 +0530

    string

---
+++ src/string/PalindromicSubstring.java
@@ -0,0 +1,29 @@
+package string;
+import java.lang.*;
+
+
+public class PalindromicSubstring {
+    public static boolean isPalindrome(String s){
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String str="abba";
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1;j<str.length();j++){
+                if(isPalindrome(str)==true);
+                System.out.print(str.substring(i ,j)+ " ");
+                count++;
+            }
+        }
+        System.out.println("\nthe substring count is"+count);
+    }
+}
